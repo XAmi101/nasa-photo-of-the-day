9eca7fb63290c4374aeb521c1b9a2f0a04434338
XAmi101 <famousvacancy@gmail.com>
2019-08-14 20:24:10 -0400

    MVP completed

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../App.css";
+
+function Header() {
+    return (
+        <div className = "header">
+            <h1>NASA's Photo of the Day</h1>
+        </div>
+    )
+}
+
+export default Header
