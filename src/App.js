86fb83710de6396a457fd72a247d35b6f1696683
XAmi101 <famousvacancy@gmail.com>
2019-08-14 20:23:51 -0400

    MVP completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
 import Nasa from "./components/Nasa.js";
+import Header from "./components/header.js";
+import Footer from "./components/footer.js";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Nasa />
+
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label="rocket">🚀!</span>
       </p>
+      <Footer/>
     </div>
+
   );
 }
 
